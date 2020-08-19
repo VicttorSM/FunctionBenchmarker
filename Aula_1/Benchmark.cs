9f2c39e7c4455c5d56e111bd6966b9af226a962f
Victtor Mendes <victtordasilva@gmail.com>
2020-08-18 23:53:14 -0300

    First Commit

---
+++ Aula_1/Benchmark.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace Aula_1
+{
+    /// <summary>
+    /// Class dedicated to execute benchmarks of functions
+    /// </summary>
+    class Benchmark
+    {
+
+        /// <summary>
+        /// Calculates the SpeedUp value between 2 functions
+        /// </summary>
+        /// <param name="baseFunctionTime">The base function</param>
+        /// <param name="otherFunctionTime">The function it will be comparing to</param>
+        /// <returns>Value of the SpeedUp of the otherFunction compared to the baseFunction</returns>
+        public static double SpeedUpCalculator(double baseFunctionTime, double otherFunctionTime)
+        {
+            return baseFunctionTime / otherFunctionTime;
+        }
+
+        /// <summary>
+        /// Calculates the average time a function takes to execute
+        /// </summary>
+        /// <param name="function">The function to be benchmarked</param>
+        /// <param name="parameter">The parameter the function takes</param>
+        /// <param name="times">The number of times the function will be executed</param>
+        /// <returns>The average time in milliseconds it took to execute the function</returns>
+        public static double AverageBenchmark(Func<int, bool> function, int parameter, int times)
+        {
+            List<double> individualBenchResults = new List<double>();
+            for (int i = 0; i < times; i++)
+            {
+                individualBenchResults.Add(BenchmarkFunction(function, parameter));
+            }
+            return individualBenchResults.Average();
+        }
+
+        /// <summary>
+        /// Calculates the time a function takes to execute once
+        /// </summary>
+        /// <param name="function">The function to be benchmarked</param>
+        /// <param name="parameter">The parameter the function takes</param>
+        /// <returns>The time in milliseconds it took to execute the function</returns>
+        public static double BenchmarkFunction(Func<int, bool> function, int parameter)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            function(parameter);
+            return stopwatch.Elapsed.TotalMilliseconds;
+        }
+
+        /// <summary>
+        /// Executes the whole benchmark, and returns its results
+        /// </summary>
+        /// <param name="functions">List of functions to be benchmarked</param>
+        /// <param name="parameterValues">List of parameters each functions will take in order to execute</param>
+        /// <param name="times">Number of times each function will execute with the same parameter</param>
+        /// <returns>A list of dictionaries, each dictionary in the list represents the benchmark records from each function</returns>
+        public static List<Dictionary<int, double>> ExecuteFullBenchmark(List<Func<int, bool>> functions, List<int> parameterValues, int times = 1)
+        {
+            // Starting from 0, each dictionary in the list represents the benchmark records from each function to be tested
+            List<Dictionary<int, double>> benchmarkList = new List<Dictionary<int, double>>();
+
+            for (int i = 0; i < functions.Count; i++)
+            {
+                // Initializes a new dictionary for this function
+                benchmarkList.Add(new Dictionary<int, double>());
+                var function = functions[i];
+
+                // Adds the benchmark results for each parameter to this dictionary
+                foreach (int parameter in parameterValues)
+                {
+                    benchmarkList[i].Add(parameter, AverageBenchmark(function, parameter, times));
+                }
+            }
+
+            return benchmarkList;
+        }
+
+        /// <summary>
+        /// Calculates the responses of the function for each parameter and returns them
+        /// </summary>
+        /// <param name="function">The function that will determine the answer</param>
+        /// <param name="parameterValues">The parameters this function will be taking</param>
+        /// <returns>Responses for each parameter used</returns>
+        public static Dictionary<int, bool> FunctionReturn(Func<int, bool> function, List<int> parameterValues)
+        {
+            // This dictionary will store the results of each parameter for this function
+            Dictionary<int, bool> dict = new Dictionary<int, bool>();
+
+            foreach (var parameter in parameterValues)
+            {
+                dict.Add(parameter, function(parameter));
+            }
+
+            return dict;
+        }
+    }
+}
---
+++ Aula_1/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Aula_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Func<int, bool>> functions = new List<Func<int, bool>>
+            {
+                TestaPrimo.TestaPrimo1Del,
+                TestaPrimo.TestaPrimo2Del,
+                TestaPrimo.TestaPrimo3Del,
+                TestaPrimo.TestaPrimo4Del
+            };
+
+            List<int> parameters = new List<int>
+            {
+                7,
+                27,
+                8421,
+                13033,
+                524287,
+                664283,
+                2147483647
+            };
+
+            // The benchmark will not start if there are no functions to execute
+            if (functions.Count == 0)
+            {
+                Console.WriteLine("No functions to execute...");
+                Console.ReadLine();
+                return;
+            }
+
+            // Asks for user input to the "times" variable
+            int times;
+            Console.Write("Enter the number of times each function will be executed: ");
+            while (!int.TryParse(Console.ReadLine(), out times))
+            {
+                Console.WriteLine("Invalid value");
+                Console.Write("Enter the number of times each function will be executed: ");
+            }
+
+
+            Console.WriteLine($"Executing benchmark {times} {(times == 1 ? "time" : "times")} for each function");
+            // All the heavy processing happens in these 2 lines
+            var benchList = Benchmark.ExecuteFullBenchmark(functions, parameters, times);
+            var functionAnswers = Benchmark.FunctionReturn(functions[0], parameters);
+
+
+            // Here the results are displayed for each parameter
+            foreach (var parameter in parameters)
+            {
+                Console.WriteLine($"Parameter: {parameter}");
+
+                Console.WriteLine($"Prime: {functionAnswers[parameter]}");
+
+                // Prints the average elapsed time for each function in this parameter
+                for (int i = 0; i < functions.Count; i++)
+                {
+                    var functionNumber = i + 1;
+                    var elapsedTime = benchList[i][parameter];
+
+                    Console.WriteLine($"Funtion {functionNumber} elapsed time: {elapsedTime} milliseconds");
+                }
+
+
+                // Prints the speedups
+                var baseSpeed = benchList[0][parameter];
+                for (int i = 1; i < functions.Count; i++)
+                {
+                    var functionNumber = i + 1;
+                    var speedup = Benchmark.SpeedUpCalculator(baseSpeed, benchList[i][parameter]);
+                    Console.WriteLine($"SpeedUp of {functionNumber} compared to function 1: {speedup}");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Press enter to close the application...");
+            Console.ReadLine();
+        }
+    }
+}
